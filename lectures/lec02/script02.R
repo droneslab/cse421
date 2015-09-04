d6453a894c59280340ed76301422204e71975566
Taylor Arnold <taylor.arnold@acm.org>
2015-09-04 00:35:33 +0000

    updates for lecture02

--- lectures/lec02/script02.R
+++ lectures/lec02/script02.R
@@ -1,3 +1,4 @@
+###################################
 # Figure normal_density.pdf
 pdf("img/normal_density.pdf", height=7.5, width=9)
 SD <- c(0.25, 0.5, 1, 2)
@@ -13,6 +14,69 @@ text(0, dnorm(0, sd=SD[4])+0.07, expression(paste(sigma," = ", 2)))
 dev.off()
 
 
-# Simulation of y = x b + error
-x <- c(rep(1,10),100)
-y <- x + rnorm(11)
+###################################
+# Simulation of simple regression
+x <- seq(0,1,by=0.01)
+n <- length(x)
+beta <- 1
+sigma <- 0.25
+
+# get one sample of y and plot it with
+# ols estimator
+y <- x * beta + rnorm(n,sd=sigma)
+plot(x,y,pch=19)
+betaHat <- sum(y*x) / sum(x^2)
+abline(0,betaHat,col="red")
+
+# run 1e3 samples
+N <- 1000
+betaHat <- rep(NA, N)
+for (i in 1:N) {
+  y <- x * beta + rnorm(n,sd=sigma)
+  betaHat[i] <- sum(y*x) / sum(x^2)
+}
+
+# check that results hold for large N
+hist(betaHat,breaks=100)
+mean(betaHat) - beta
+var(betaHat) - sigma^2 / sum(x^2)
+
+# leverage
+x[n] <- 10
+y <- x * beta + rnorm(n,sd=sigma)
+
+plot(x,y,cex=0.3)
+betaHat <- sum(y*x) / sum(x^2)
+abline(0,betaHat,col="red")
+
+# leverage with corruption
+x[n] <- 10
+y <- x * beta + rnorm(n,sd=sigma)
+k <- n
+y[k] <- y[k] - 1
+
+plot(x,y,cex=0.3)
+betaHat <- sum(y*x) / sum(x^2)
+abline(0,betaHat,col="red")
+betaHatMinus <- sum(y[-k]*x[-k]) / sum(x[-k]^2)
+abline(0,betaHatMinus,col="blue")
+
+# calc leverage measure of each point
+x[n] <- 10
+y <- x * beta + rnorm(n,sd=sigma)
+
+results <- rep(NA, n)
+for (k in 1:n) {
+  ynew <- y
+  ynew[k] <- ynew[k] - 1
+
+  betaHat <- sum(ynew*x) / sum(x^2)
+  betaHatMinus <- sum(ynew[-k]*x[-k]) / sum(x[-k]^2)
+  results[k] <- betaHatMinus - betaHat
+}
+
+plot(x,results)
+
+
+
+
