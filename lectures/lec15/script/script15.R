ce72c1858db9d6bdadf26950e1aaf2d52474ffa0
taylor <taylor.arnold@acm.org>
2015-11-03 19:46:04 -0500

    update pset05

--- lectures/lec15/script/script15.R
+++ lectures/lec15/script/script15.R
@@ -1,21 +1,49 @@
-
-
 ####################
 # Columbia images
 library(jpeg)
-pm <- read.csv("photoMetaData.csv")
+pm <- read.csv("photoMetaData.csv", as.is=TRUE)
 
-table(pm$location)
-table(pm$photographer)
+system(paste0("open columbiaImages/",pm$name[94]))
 table(pm$category)
 
-system(paste0("open columbiaImages/",pm$name[300]))
+pm <- pm[pm$category != "artificial",]
+pm <- pm[pm$category != "natural",]
+n <- nrow(pm)
 
-img <- readJPEG(paste0("columbiaImages/",pm$name[300]))
-class(img)
-dim(img)
+y <- rep(0, nrow(pm))
+y[grep("indoor", pm$category)] <- 1
 
-options(digits=4)
+set.seed(1)
+tFlag <- as.numeric(runif(n) > 0.5)
+
+X <- matrix(0,nrow=n,ncol=3)
+for (j in 1:n) {
+  img <- readJPEG(paste0("columbiaImages/",pm$name[j]))
+  X[j,] <- apply(img, 3, median)
+  print(j)
+}
+
+y1 <- y[tFlag == 1]
+y2 <- y[tFlag == 0]
+X1 <- X[tFlag == 1,]
+X2 <- X[tFlag == 0,]
+
+betaHat <- qr.solve(crossprod(X1),crossprod(X1,y1))
+
+y2Hat <- X2 %*% betaHat
+table(y2Hat > 0.5, y2)
+
+mean((y2Hat > 0.5) == y2)
+mean(0 == y2)
+
+
+X <- matrix(0,nrow=n,ncol=3)
+for (j in 1:n) {
+  img <- readJPEG(paste0("columbiaImages/",pm$name[j]))
+  X[j,] <- apply(img, 3, median)
+  print(j)
+}
+
 
 
 
