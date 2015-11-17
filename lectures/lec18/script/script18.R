48d92852a994f64ed6e2807b13eb232898c8598d
taylor <taylor.arnold@acm.org>
2015-11-17 14:48:13 -0500

    update

--- lectures/lec18/script/script18.R
+++ lectures/lec18/script/script18.R
@@ -1,26 +1,36 @@
-##################### Photograph examples
+### Photograph examples
+
+## Read in the library and metadata
 library(jpeg)
 pm <- read.csv("photoMetaData.csv")
-pm <- pm[pm$category %in% c("artificial","natural"),]
 n <- nrow(pm)
 
 trainFlag <- (runif(n) > 0.5)
-y <- as.numeric(pm$category == "natural")
+y <- as.numeric(pm$category == "outdoor-day")
 
 X <- matrix(NA, ncol=3, nrow=n)
 for (j in 1:n) {
   img <- readJPEG(paste0("columbiaImages/",pm$name[j]))
-  X[j,] <- apply(img,3,mean)
+  X[j,] <- apply(img,3,median)
   print(sprintf("%03d / %03d", j, n))
 }
+#saveRDS(X, "median_rgb.Rds")
+X <- readRDS("median_rgb.Rds")
 
+# build a glm model on these median values
 out <- glm(y ~ X, family=binomial, subset=trainFlag)
+out$iter
 summary(out)
 
+# How well did we do?
 pred <- 1 / (1 + exp(-1 * cbind(1,X) %*% coef(out)))
 y[order(pred)]
+y[!trainFlag][order(pred[!trainFlag])]
 
-### ROC curve (see lecture 12)
+mean((as.numeric(pred > 0.5) == y)[trainFlag])
+mean((as.numeric(pred > 0.5) == y)[!trainFlag])
+
+## ROC curve (see lecture 12)
 roc <- function(y, pred) {
   alpha <- quantile(pred, seq(0,1,by=0.01))
   N <- length(alpha)
@@ -39,31 +49,68 @@ r <- roc(y[!trainFlag], pred[!trainFlag])
 plot(r$fpr, r$tpr, xlab="false positive rate", ylab="true positive rate", type="l")
 abline(0,1,lty="dashed")
 
+# auc
 auc <- function(r) {
   sum((r$fpr) * diff(c(0,r$tpr)))
 }
+glmAuc <- auc(r)
+glmAuc
 
-auc(r)
+## ridge regression?
+library(glmnet)
+outRidge <- glmnet(X[trainFlag,], y[trainFlag], family="binomial",
+                    alpha=0)
+
+aucValsRidge <- rep(NA, length(outRidge$lambda))
+for (j in 1:length(outRidge$lambda)) {
+  pred <- 1 / (1 + exp(-1 * cbind(1,X) %*% coef(outRidge)[,j]))
+  r <- roc(y[trainFlag == 0], pred[trainFlag == 0])
+  aucValsRidge[j] <- auc(r)
+}
+
+plot(log(outRidge$lambda)[-1], aucValsRidge[-1], type="l",
+      xlab="log lambda", ylab="AUC", main="ridge",
+      ylim=range(c(aucValsRidge[-1],glmAuc)))
+abline(h=glmAuc,col="salmon")
+text(log(outRidge$lambda)[50],glmAuc-0.003,"glm value",col="salmon")
+
+## lasso regression?
+outLasso <- glmnet(X[trainFlag,], y[trainFlag], family="binomial",
+                    alpha=1)
+
+aucValsLasso <- rep(NA, length(outLasso$lambda))
+for (j in 1:length(outLasso$lambda)) {
+  pred <- 1 / (1 + exp(-1 * cbind(1,X) %*% coef(outLasso)[,j]))
+  r <- roc(y[trainFlag == 0], pred[trainFlag == 0])
+  aucValsLasso[j] <- auc(r)
+}
+
+plot(log(outLasso$lambda)[-1], aucValsLasso[-1], type="l",
+      xlab="log lambda", ylab="AUC", main="lasso",
+      ylim=range(c(aucValsLasso[-1],glmAuc)))
+abline(h=glmAuc,col="salmon")
+text(log(outLasso$lambda)[50],glmAuc-0.0012,"glm value",col="salmon")
 
 # What are worst and best examples?
+pred <- 1 / (1 + exp(-1 * cbind(1,X) %*% coef(out)))
 temp <- cbind(pm[order(pred),],y[order(pred)])
 
+# outdoor image that looks like it is not
 id <- which(temp$y == 1)[1]
 system(paste0("open columbiaImages/",temp$name[id]))
 
-id <- rev(which(temp$y == 0))[1]
+# non-outdoor image that looks like it is not
+id <- max(which(temp$y == 0))
 system(paste0("open columbiaImages/",temp$name[id]))
 
-id <- which(temp$y == 0)[1]
+# "most" outdoor-like image
+id <- max(which(temp$y == 1))
 system(paste0("open columbiaImages/",temp$name[id]))
 
-id <- rev(which(temp$y == 1))[1]
-system(paste0("open columbiaImages/",temp$name[id]))
+########
 
-#############################
-### A bit more grainularity
-img <- readJPEG(paste0("columbiaImages/",pm$name[100]))
-system(paste0("open columbiaImages/",pm$name[100]))
+img <- readJPEG(paste0("columbiaImages/",pm$name[500]))
+system(paste0("open columbiaImages/",pm$name[500]))
 
 hist(img[,,1],breaks=seq(0,1,by=0.05))
 
@@ -72,53 +119,87 @@ hist(img[,,1],breaks=seq(0,1,by=0.05), col="#ff000099",border=NA)
 hist(img[,,2],breaks=seq(0,1,by=0.05), col="#00ff0099",border=NA,add=TRUE)
 hist(img[,,3],breaks=seq(0,1,by=0.05), col="#0000ff99",border=NA,add=TRUE)
 
-# We can save the output of the historam and use it as
-# features in the model
-h <- hist(img[,,1],breaks=seq(0,1,by=0.05), plot=FALSE)
-h$density
+########
 
-X2 <- matrix(NA, ncol=3*20, nrow=n)
+X <- matrix(NA, ncol=3*21, nrow=n)
 for (j in 1:n) {
   img <- readJPEG(paste0("columbiaImages/",pm$name[j]))
-  h <- apply(img,3,hist,breaks=seq(0,1,by=0.05), plot=FALSE)
-  h <- Map(`getElement`, h, "density")
-  X2[j,] <- unlist(h)
+  h <- apply(img,3,quantile,probs=seq(0,1,by=0.05))
+  X[j,] <- as.numeric(h)
   print(sprintf("%03d / %03d", j, n))
 }
+saveRDS(X, "quantile_rgb.Rds")
+#X <- readRDS("quantile_rgb.Rds")
 
-# Why are there NAs?
-out2 <- glm(y ~ X2, family=binomial, subset=trainFlag)
-summary(out2)
+out <- glm(y ~ X, family=binomial, subset=trainFlag)
+pred <- 1 / (1 + exp(-1 * cbind(1,X) %*% coef(out)))
+glmAuc # old
+auc(roc(y[!trainFlag], pred[!trainFlag])) # new
 
-X3 <- X2[,c(1:9,11:19,21:29)]
+glmAuc <- auc(roc(y[trainFlag == 0], pred[trainFlag == 0]))
 
-out3 <- glm(y ~ X3, family=binomial, subset=trainFlag)
-summary(out3)
+# ridge regression now?
+outRidge <- glmnet(X[trainFlag == 1,], y[trainFlag == 1],
+                    family="binomial", alpha=0)
 
-pred3 <- 1 / (1 + exp(-1 * cbind(1,X3) %*% coef(out3)))
-y[order(pred3)]
+aucValsRidge <- rep(NA, length(outLasso$lambda))
+for (j in 1:length(outRidge$lambda)) {
+  pred <- 1 / (1 + exp(-1 * cbind(1,X) %*% coef(outRidge)[,j]))
+  r <- roc(y[trainFlag == 0], pred[trainFlag == 0])
+  aucValsRidge[j] <- auc(r)
+}
 
-r2 <- roc(y, pred2)
-plot(r2$fpr, r2$tpr, xlab="false positive rate",
-     ylab="true positive rate", type="l", col="blue")
-lines(r$fpr, r$tpr)
+plot(log(outRidge$lambda)[-1], aucValsRidge[-1], type="l",
+      xlab="log lambda", ylab="AUC", main="ridge",
+      ylim=range(c(aucValsRidge[-1],glmAuc)))
+abline(h=glmAuc,col="salmon")
+text(log(outRidge$lambda)[50],glmAuc-0.003,"glm value",col="salmon")
 
-auc(r)
-auc(r2)
+## lasso regression?
+outLasso <- glmnet(X[trainFlag,], y[trainFlag], family="binomial",
+                   alpha=1)
 
-######## ridge regression?
-library(glmnet)
+aucValsLasso <- rep(NA, length(outLasso$lambda))
+for (j in 1:length(outLasso$lambda)) {
+  pred <- 1 / (1 + exp(-1 * cbind(1,X) %*% coef(outLasso)[,j]))
+  r <- roc(y[trainFlag == 0], pred[trainFlag == 0])
+  aucValsLasso[j] <- auc(r)
+}
 
-out <- glmnet(X[trainFlag,], y[trainFlag], family="binomial", alpha=0)
-ridgeP <- predict(out,newx=X2[!trainFlag,], type="response")
-dim(ridgeP)
+plot(log(outLasso$lambda)[-1], aucValsLasso[-1], type="l",
+      xlab="log lambda", ylab="AUC", main="lasso",
+      ylim=range(c(aucValsLasso[-1],glmAuc)))
+abline(h=glmAuc,col="salmon")
+text(log(outLasso$lambda)[50],glmAuc-0.0012,"glm value",col="salmon")
 
-r3 <- roc(y[!trainFlag], ridgeP[,2])
-plot(r2$fpr, r2$tpr, xlab="false positive rate",
-     ylab="true positive rate", type="l", col="blue")
-lines(r$fpr, r$tpr)
-lines(r3$fpr, r3$tpr, col="salmon")
+########
 
+# visualizing this all?
 
+SVD <- svd(X)
+U <- SVD$u
 
+plot(U[,1], U[,2], col=y+1, pch=19, cex=0.5)
+plot(U[,2], U[,3], col=y+1, pch=19, cex=0.5)
 
+U[,1] <- U[,1] - min(U[,1])
+U[,1] <- U[,1] / max(U[,1])
+U[,2] <- U[,2] - min(U[,2])
+U[,2] <- U[,2] / max(U[,2])
+
+set.seed(1)
+par(mar=c(0,0,0,0))
+rho <- 0.1
+plot(0,0,type="n", xlim=c(0,1+rho), ylim=c(0,1+rho))
+
+files <- paste0("columbiaImages/",pm$name)
+for (j in sample(1:length(files))) {
+  z <- readJPEG(files[j])
+
+  rat <- dim(z)[1] / dim(z)[2]
+  delta_x <- ifelse(rat > 1, 1, rat) * rho
+  delta_y <- ifelse(rat > 1, rat, 1) * rho
+  rasterImage(z, xleft=U[j,1], ybottom=U[j,2],
+                 xright=U[j,1]+delta_x, ytop=U[j,2]+delta_y)
+  rect(U[j,1],U[j,2],U[j,1]+delta_x,U[j,2]+delta_y,lwd=2)
+}
