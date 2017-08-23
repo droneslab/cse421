4912283f925e3c2f25dc855c22ace02479220c65
Karthik Dantu <kdantu@buffalo.edu>
2017-08-22 22:14:41 -0400

    deleted stats class stuff

--- psets/pset03/script/clean_script.R
+++
@@ -1,7 +0,0 @@
-# Create dataset for the problem set
-x <- readRDS("euler.stat.yale.edu/~tba3/lectures/lec08/data/airline2007_clean.Rds")
-topArr <- names(sort(table(x$dest),decreasing=TRUE)[1:25])
-x <- x[x$dest %in% topArr,]
-x$group <- sample(c("I", "II", "III"), nrow(x), replace=TRUE)
-saveRDS(x, "airline2007_pset03.Rds")
-
